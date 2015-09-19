16f20dff5fe4de18603ad178360cf58694b9bf08
Yuri Numerov <achi@hush.ai>
2015-09-19 20:19:40 +0200

    added doorbell functionality

--- mailding.ino
+++ mailding.ino
@@ -3,18 +3,24 @@ int switchin=3;
 int switchout=5;
 int onled=13;
 int mailsensor=2;
+int doorbell=4;
+int doorbellsig=6;
 
 void setup() {                
   pinMode(mailled, OUTPUT);     
   pinMode(switchin, INPUT);
   pinMode(switchout,OUTPUT);
   pinMode(onled, OUTPUT);
+  pinMode(doorbell, INPUT);
+  pinMode(doorbellsig, OUTPUT);
+  pinMode()
   digitalWrite(switchout, HIGH);
 }
 
 void loop() {
   
   int switchstatus=digitalRead(switchin);
+
   if (switchstatus==1){
     digitalWrite(onled, HIGH);
     if (digitalRead(mailsensor)==0){
@@ -24,5 +30,9 @@ void loop() {
 
   else{
     digitalWrite(onled,LOW);
+  }
+
+  if(digitalRead(doorbell)==1){
+    digitalWrite(doorbellsig, HIGH);
   }
 }
